f4a136c7611cce08b72587d95a0c8f5b768fabe3
SeaStar <1811230375@qq.com>
2023-06-27 10:49:40 +0800

    Initial commit

---
+++ src/client/java/top/seastar/TestModClient.java
@@ -0,0 +1,10 @@
+package top.seastar;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
